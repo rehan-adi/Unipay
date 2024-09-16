6a246d6159e07eb15958c6f564c2a008ec3ae587
Rehan <rehanalire52@gmail.com>
2024-09-16 15:31:24 +0530

    style(page): add font-semibold class to enhance text styling

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
        <div className="bg-black text-white flex h-screen justify-center flex-col items-center">
-           <h1 className="text-3xl">Turberepo Project</h1>
+           <h1 className="text-3xl font-semibold">Turberepo Project</h1>
            <h3 className="mt-3 text-base">With Prisma and Next JS Fullstack</h3>
        </div>
   );
