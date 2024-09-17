570e19b4f31d6ffab4d0abbe6e1ec232088435c0
Rehan <rehanalire52@gmail.com>
2024-09-17 16:19:34 +0530

    Add next auth with Credentials Provider

---
+++ apps/web/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,67 @@
+import bcrypt from "bcrypt";
+import db from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+    providers: [
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            phone: { label: "Phone number", type: "text", placeholder: "Enter your Phone number" },
+            password: { label: "Password", type: "password", placeholder: "Enter your Password" }
+          },
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
+
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.username,
+                        email: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword,
+                        username: credentials.username, 
+                        email: credentials.email,   
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    username: user.username,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        // TODO: can u fix the type here? Using any is bad
+        async session({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
+  }
+ 
